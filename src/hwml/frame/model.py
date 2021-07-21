6078fbeca4e4e9054657bc4e0236f006b9d2e241
zhmhbest <zhmhbest@gmail.com>
2021-07-21 19:21:39 +0800

    update

--- src/hwml/frame/model.py
+++ src/hwml/frame/model.py
@@ -1,4 +1,39 @@
+from abc import abstractmethod
+from typing import Union
+
+from numpy import ndarray
+
+from .loss import Loss
+from .optimizer import Optimizer
+from .layer import Layer, ParameterLayer
 
 
 class Model:
-    pass
+    def __init__(self):
+        self.layers = []
+        self.loss: Union[None, Loss] = None
+        self.optimizer: Union[None, Optimizer] = None
+
+    def add(self, layer: Layer):
+        self.layers.append(layer)
+
+    def set_loss(self, loss: Loss):
+        self.loss = loss
+
+    def set_optimizer(self, optimizer: Optimizer):
+        self.optimizer = optimizer
+        for layer in self.layers:
+            if isinstance(layer, ParameterLayer):
+                layer.optimizer = optimizer
+
+    def __call__(self, x: ndarray):
+        return self.forward(x)
+
+    def forward(self, x: ndarray):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    @abstractmethod
+    def train(self, x_train: ndarray, y_train: ndarray):
+        raise NotImplementedError()
--- src/hwml/nn/models.py
+++ src/hwml/nn/models.py
@@ -1,35 +1,9 @@
-from typing import Union
-
 from numpy import ndarray
-from hwml.frame import Model, Layer, Loss, Optimizer, ParameterLayer
+
+from hwml.frame import Model, ParameterLayer
 
 
 class Sequential(Model):
-    def __init__(self):
-        self.layers = []
-        self.loss: Union[None, Loss] = None
-        self.optimizer: Union[None, Optimizer] = None
-
-    def add(self, layer: Layer):
-        self.layers.append(layer)
-
-    def set_loss(self, loss: Loss):
-        self.loss = loss
-
-    def set_optimizer(self, optimizer: Optimizer):
-        self.optimizer = optimizer
-        for layer in self.layers:
-            if isinstance(layer, ParameterLayer):
-                layer.optimizer = optimizer
-
-    def __call__(self, x: ndarray):
-        return self.forward(x)
-
-    def forward(self, x: ndarray):
-        for layer in self.layers:
-            x = layer(x)
-        return x
-
     def train(self, x_train: ndarray, y_train: ndarray):
         assert self.optimizer is not None
         assert self.loss is not None
